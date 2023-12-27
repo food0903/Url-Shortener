0e40341ee4f3f09e19037a4d023ede6cdb8517de
food0903 <phuclthg@gmail.com>
2023-12-26 20:03:55 -0800

    added endpoints

--- server.py
+++ server.py
@@ -1,90 +1,37 @@
 import sqlite3
 from fastapi import FastAPI
 import uvicorn
-from datetime import datetime
-from pydantic import BaseModel
+from sqlite_helpers import *
+
 
 app = FastAPI()
-connection_obj = sqlite3.connect('url.db')
-cursor_obj = connection_obj.cursor()
+DATABASE = 'url.db'
 
 
-# Creating table
-table = """ CREATE TABLE IF NOT EXISTS URLTable (
-            id INTEGER NOT NULL PRIMARY KEY,
-            URL VARCHAR(255) NOT NULL,
-            Alias VARCHAR(25) NOT NULL UNIQUE,
-            timestamp TIMESTAMP
-        ); """
-
-cursor_obj.execute(table)
-
-print("Table is Ready")
-
-# Close the connection
-connection_obj.close()
+@app.get("/")
+def root():
+    return ({"message": "hello world"})
 
 
-# insert url and their alias into the table
-def insert_url(url, alias):
-    conn = sqlite3.connect('url.db')
-    cursor = conn.cursor()
-
-    query = ''' INSERT INTO URLTable (URL, Alias, timestamp)
-                VALUES (?, ?, ?) '''
-
-    current_timestamp = datetime.now()
-
-    cursor.execute(query, (url, alias, current_timestamp))
-
-    conn.commit()
-
-    conn.close()
-
-# delete url given alias
+@app.get("/urls")
+def get_all_urls():
+    return list_all_urls(DATABASE)
 
 
-def delete_url(alias):
-    conn = sqlite3.connect('url.db')
-    cursor = conn.cursor()
-
-    query = ''' DELETE FROM URLTable WHERE Alias = ? '''
-
-    cursor.execute(query, (alias,))
-
-    conn.commit()
-
-    conn.close()
-
-# get all urls in the table
+@app.get("/url")
+def alias_url(alias):
+    return get_url_by_alias(alias, DATABASE)
 
 
-def list_all_urls():
-    conn = sqlite3.connect('url.db')
-    cursor = conn.cursor()
-
-    query = ''' SELECT * FROM URLTable '''
-    cursor.execute(query)
-
-    urls = cursor.fetchall()
-
-    # Close the connection
-    conn.close()
-
-    return urls
-
-# get a specific url given the alias
+@app.post('/insert')
+def add_url(url, alias):
+    return insert_url(url, alias, DATABASE)
 
 
-def get_url_by_alias(alias):
-    conn = sqlite3.connect('url.db')
-    cursor = conn.cursor()
+@app.delete('/delete')
+def add_url(alias):
+    return delete_url(alias, DATABASE)
 
-    query = ''' SELECT * FROM URLTable WHERE Alias = ? '''
-    cursor.execute(query, (alias,))
 
-    url_record = cursor.fetchone()
-
-    conn.close()
-
-    return url_record
+if __name__ == "__main__":
+    uvicorn.run("server:app", port=8000, reload=True)
---
+++ sqlite_helpers.py
@@ -0,0 +1,86 @@
+import sqlite3
+from datetime import datetime
+
+connection_obj = sqlite3.connect('url.db')
+cursor_obj = connection_obj.cursor()
+
+
+# Creating table
+table = """ CREATE TABLE IF NOT EXISTS URLTable (
+            id INTEGER NOT NULL PRIMARY KEY,
+            URL VARCHAR(255) NOT NULL,
+            Alias VARCHAR(25) NOT NULL UNIQUE,
+            timestamp TIMESTAMP
+        ); """
+
+cursor_obj.execute(table)
+
+print("Table is Ready")
+
+# Close the connection
+connection_obj.close()
+
+
+# insert url and their alias into the table
+def insert_url(url, alias, db):
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    query = ''' INSERT INTO URLTable (URL, Alias, timestamp)
+                VALUES (?, ?, ?) '''
+
+    current_timestamp = datetime.now()
+
+    cursor.execute(query, (url, alias, current_timestamp))
+
+    conn.commit()
+
+    conn.close()
+
+# delete url given alias
+
+
+def delete_url(alias, db):
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    query = ''' DELETE FROM URLTable WHERE Alias = ? '''
+
+    cursor.execute(query, (alias,))
+
+    conn.commit()
+
+    conn.close()
+
+# get all urls in the table
+
+
+def list_all_urls(db):
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    query = ''' SELECT * FROM URLTable '''
+    cursor.execute(query)
+
+    urls = cursor.fetchall()
+
+    # Close the connection
+    conn.close()
+
+    return urls
+
+# get a specific url given the alias
+
+
+def get_url_by_alias(alias, db):
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    query = ''' SELECT * FROM URLTable WHERE Alias = ? '''
+    cursor.execute(query, (alias,))
+
+    url_record = cursor.fetchone()
+
+    conn.close()
+
+    return url_record
